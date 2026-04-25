c1b42e9d40b6060e9357d04ddca68e7c45637db6
Hsaadk <Hshahsaad@gmail.com>
2026-04-26 03:15:00 +0500

    updated styles.css

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 @echo off
-start cmd /k "cd /d C:\Users\Asim Saeed\Desktop\iris-auth-mainn\iris-auth-main\backend && node server.js"
-start cmd /k "cd /d C:\Users\Asim Saeed\Desktop\iris-auth-mainn\iris-auth-main && npm run dev"
+start cmd /k "cd /d C:\Users\Laptop Valley\iris-auth-main\backend && node server.js"
+start cmd /k "cd /d C:\Users\Laptop Valley\iris-auth-main && npm run dev"
